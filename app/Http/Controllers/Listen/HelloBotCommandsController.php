693a362e0e3a03a2a4e4bff6723780f77876cfba
davidcoroian <david.coroian@code.berlin>
2018-03-21 23:13:05 +0100

    Function hearsMood will now send a direct message to a user that used the keyword mood and ask him about his mood. This function is currently replaced by handleMood which starts a conversation

--- app/Http/Controllers/Listen/HelloBotCommandsController.php
+++ app/Http/Controllers/Listen/HelloBotCommandsController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers\Listen;
 
+use App\Conversations\ExampleConversation;
+use App\Conversations\MoodImputConversation;
 use App\Http\Controllers\Controller;
+use BotMan\BotMan\Messages\Outgoing\Actions\Button;
+use BotMan\BotMan\Messages\Outgoing\Question;
+use BotMan\Drivers\Slack\SlackDriver;
 
 class HelloBotCommandsController extends Controller
 {
@@ -12,7 +17,7 @@ class HelloBotCommandsController extends Controller
      */
     public function handleSayHello($bot)
     {
-        $bot->reply("Hello, I'm Mood bot!");
+        $bot->reply("I am mood bot");
     }
 
     /**
@@ -24,10 +29,32 @@ class HelloBotCommandsController extends Controller
     {
         $bot->reply("Hello, $name. I'm Mood bot");
     }
-
+    //replace for now by function handleMood
     public function hearsMoods($bot)
     {
         $bot->reply("Would you like to input your mood for today?");
+
+        $question = Question::create('How are you feeling today?')
+            ->addButtons([
+                Button::create('Happy :grin:')->value('5'),
+                Button::create('Neutral :neutral_face:')->value('4'),
+                Button::create('Sick :face_with_thermometer:')->value('3'),
+                Button::create('Angry :angry:')->value('2'),
+                Button::create('Sad :pensive:')->value('1'),
+                Button::create('Unamused :unamused:')->value('3'),
+            ]);
+
+
+        $user = $bot->getUser();
+        // Access ID
+        $id = $user->getId();
+
+        $bot->say($question, $id, SlackDriver::class);
+    }
+
+    public function handleMood($bot)
+    {
+        $bot->startConversation(new MoodImputConversation);
     }
 
 }
