dcb2368bbcb1361328d741fa614f671a09d24b37
davidcoroian <david.coroian@code.berlin>
2018-03-21 13:45:33 +0100

    Added message buttons to the message that is being send to slack everyday to grab the users mood

--- app/Console/Commands/SendSlackMessage.php
+++ app/Console/Commands/SendSlackMessage.php
@@ -3,11 +3,14 @@
 namespace App\Console\Commands;
 
 use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Messages\Outgoing\Actions\Button;
+use BotMan\Drivers\Slack\Extensions\Menu;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 use App\Conversations\ExampleConversation;
-
+use \BotMan\Drivers\Slack\SlackDriver;
+use \BotMan\BotMan\Messages\Outgoing\Question;
 
 /* As filename states, this is a COMMAND! There is no view for this file!
    The Command to be used to activate the function below is php artisan schedule:daemon
@@ -42,82 +45,19 @@ class SendSlackMessage extends Command
     public function handle()
     {
 
-        /* Incoming webhook URL - Payload is sent to URL that is provided by Slack and then Slack API
-           transforms the payload to Slack message
-        */
         $botman = app('botman');
 
-        //$botman->say("Would you like to input your mood for today?");
+        $question = Question::create('How are you feeling today?')
+            ->addButtons([
+                    Button::create('Happy :grin:')->value('5'),
+                    Button::create('Neutral :neutral_face:')->value('4'),
+                    Button::create('Sick :face_with_thermometer:')->value('3'),
+                    Button::create('Angry :angry:')->value('2'),
+                    Button::create('Sad :pensive:')->value('1'),
+                    Button::create('Unamused :unamused:')->value('3'),
+            ]);
 
-        $botman->say('help', 'C9FHM6Q1X', SlackDriver::class);
-
-        //$botman->listen();
-//        $url = 'https://hooks.slack.com/services/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';
-//
-//        //prepares the message to be sent to slack via incoming webhooks as JSON
-//        $payload = '{
-//            "text": "Hey! Would you like to introduce your input for today?",
-//            "attachments": [
-//                {
-//                    "text": "Please choose one of the moods displayed below",
-//                    "fallback": "You are unable to choose a game",
-//                    "callback_id": "wopr_game",
-//                    "color": "#3AA3E3",
-//                    "attachment_type": "default",
-//                    "actions": [
-//                        {
-//                            "name": "mood",
-//                            "text": "Happy :grin:",
-//                            "type": "button",
-//                            "value": "5"
-//                        },
-//                        {
-//                            "name": "mood",
-//                            "text": "Angry :angry:",
-//                            "type": "button",
-//                            "value": "1.5",
-//                        },
-//                        {
-//                            "name": "mood",
-//                            "text": "Neutral :neutral_face:",
-//                            "type": "button",
-//                            "value": "2.5"
-//                        },
-//                        {
-//                            "name": "mood",
-//                            "text": "Sick :face_with_head_bandage:",
-//                            "type": "button",
-//                            "value": "2"
-//                        },
-//                        {
-//                            "name": "mood",
-//                            "text": "Sad :pensive:",
-//                            "style": "danger",
-//                            "type": "button",
-//                            "value": "1",
-//                            "confirm": {
-//                                "title": "We are sad to hear that.",
-//                                "text": "Would you like to tell me why?",
-//                                "ok_text": "Yes sure",
-//                                "dismiss_text": "No"
-//                            }
-//                        }
-//                    ]
-//                }
-//            ]
-//        }';
-//
-//        // curl actually sends payload to URL (otherwise function wouln't know what and where to send it)
-//
-//        $ch = curl_init( $url );
-//        curl_setopt( $ch, CURLOPT_POST, 1);
-//        curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload);
-//        curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
-//        curl_setopt( $ch, CURLOPT_HEADER, 0);
-//        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
-//
-//        $response = curl_exec( $ch );
-
+        $botman->say($question, 'C9FHM6Q1X', SlackDriver::class);
 
     }
 }
