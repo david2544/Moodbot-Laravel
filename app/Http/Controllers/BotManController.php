5eed440bfdd7dbb8bfbb47b0641a7e8725a0ba0c
davidcoroian <david.coroian@code.berlin>
2018-03-27 19:25:48 +0200

    Removed one hears command, changed error message and removed unused code

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -2,12 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use BotMan\BotMan\BotMan;
-#use BotMan\BotMan\DriverManager;
-use BotMan\BotMan\Drivers\DriverManager;
-use BotMan\Drivers\Slack\SlackDriver;
-use Illuminate\Http\Request;
-use App\Conversations\ExampleConversation;
 
 class BotManController extends Controller
 {
@@ -23,18 +17,15 @@ class BotManController extends Controller
         $botman = app('botman');
 
         $botman->hears("hello I'm {name}", 'App\Http\Controllers\Listen\HelloBotCommandsController@handleSayHelloWithName');
+
         $botman->hears('mood', 'App\Http\Controllers\Listen\HelloBotCommandsController@handleMood');
         $botman->fallback(function($bot) {
-            $bot->reply('Sorry, I did not understand these commands. Here is a list of commands I understand: ...');
+            $bot->reply('Error');
         });
+
 
         $botman->listen();
     }
 
-
-    /**
-     * Loaded through routes/botman.php
-     * @param  BotMan $bot
-     */
 
 }
